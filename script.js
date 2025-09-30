ab049b3c0c50fa015ebce87ae6f5e25e1d7181b9
Victor Maceio <victor.maceio@portalsesisp.org.br>
2025-09-30 10:18:48 -0300

    feat: instancia de jogador criada

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ class jogador{
             ID: ${this.id}
             Nome: ${this.nome}
             Pontos: ${this.pontos}
-            Status: ${this.status ? Atovo : Inativo}`)
+            Status: ${this.status ? Atovo : Inativo}
+            ----------------------------`)
     }
 }
+
+let jogador1 = new jogador(1, " Thobias", 578);
+
+console.table(jogador1)
